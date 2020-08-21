d7d6248e8fc71a50abc03a4f83c73726a3ab40aa
FangHongshu <hongshufang@hotmail.com>
2020-08-21 23:18:34 +0800

    add another solution to question 26

--- 26.py
+++ 26.py
@@ -23,6 +23,32 @@ def ComplexListClone1(headNode):
         pNode=pNode.nextNode
     return nodeDict[headNode]
 
+def ComplexListClone2(headNode):
+    if headNode==None:
+        return None
+    pNode=headNode
+    #按照N->N'的格式将整个链表改为
+    while pNode!=None:
+        cloneNode=ComplexListNode(pNode.val)
+        cloneNode.nextNode=pNode.nextNode
+        pNode.nextNode=cloneNode
+        pNode=cloneNode.nextNode
+    pNode=headNode
+    while pNode!=None:
+        if pNode.siblingNode!=None:
+            pNode.nextNode.siblingNode=pNode.siblingNode.nextNode
+        pNode=pNode.nextNode.nextNode
+    pNode=headNode
+    cloneNode=headNode.nextNode
+    res=cloneNode
+    while cloneNode.nextNode!=None:
+        pNode.nextNode=cloneNode.nextNode
+        cloneNode.nextNode=cloneNode.nextNode.nextNode
+        pNode=pNode.nextNode
+        cloneNode=cloneNode.nextNode
+    return  res
+
+
 a=ComplexListNode(1)
 b=ComplexListNode(2)
 c=ComplexListNode(3)
@@ -37,7 +63,8 @@ c.nextNode=d
 d.nextNode=e
 d.siblingNode=c
 
-f=ComplexListClone1(a)
+#f=ComplexListClone1(a)
+f=ComplexListClone2(a)
 print(f)
 
     
