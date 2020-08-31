15b5d04087ce6a7a9727e3b64e4f60dab315506a
FangHongshu <hongshufang@hotmail.com>
2020-08-31 23:47:08 +0800

    add dynamic programming solution to problem 53

--- 53.py
+++ 53.py
@@ -11,6 +11,7 @@ def RegularMate(strs,targetStr):
             return False
     return Mate(strs,targetStr)
 
+#递归法
 def Mate(strs,targetStrs):
     if strs=="" and targetStrs=="":
         return True
@@ -40,8 +41,36 @@ def Mate(strs,targetStrs):
 
     return False
 
-a="ab*a"
+
+#动归
+def MateDynamic(strs,targetStrs):
+    slen=len(strs)
+    tlen=len(targetStrs)
+    res=[[False for i in range(slen+1)] for j in range(tlen+1)]
+    res[0][0]=True
+    i=1#strs Index
+    j=1#targetStrs Index
+    while i<=slen and j<=tlen:
+        if strs[i-1]!="*":
+            if strs[i-1]==targetStrs[j-1] or strs[i-1]=='.':
+                res[j][i]=res[j-1][i-1]
+            else:
+                res[j][i]=False
+        else:
+            if strs[i-2]==targetStrs[j-1]:
+                res[j][i]=(res[j][i-2] or res[j-1][i])
+            else:
+                res[j][i]=res[j][i-2]
+        i+=1
+        j+=1
+    return res[tlen][slen]
+
+
+
+
+a="aa.a"
 b="aaa"
 print(RegularMate(a,b))
+print(MateDynamic(a,b))
         
 
