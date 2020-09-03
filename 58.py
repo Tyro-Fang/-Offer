044f18fa1facf03d26220670d012a566d8b7e787
FangHongshu <hongshufang@hotmail.com>
2020-09-03 22:26:37 +0800

    add solution 58

---
+++ 58.py
@@ -0,0 +1,12 @@
+class BinaryTree:
+    def __init__(self,val):
+        self.val=val
+        self.leftNode=None
+        self.rightNode=None
+        self.father=None
+
+def NextNode(rootNode):
+    if rootNode==None:
+        return None
+    if rootNode.leftNode==None and rootNode.rightNode==None and rootNode.father==None:
+        return None
