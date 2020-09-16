bb7ec77132d866309ad667bfb37d1b4686366ded
FangHongshu <hongshufang@hotmail.com>
2020-09-16 22:50:32 +0800

    write headpsort again;

--- 64.py
+++ 64.py
@@ -1,6 +1,21 @@
 class MaxHeap:
     def __init__(self):
         self.val=[]
+        self.length=0
+    def less(a,b):
+        if a<b:
+            return True
+        return False
+    
+    def sink(vals,n):
+        k=0
+        while 2*k+1<n:
+            temp=vals[k]
+            if less(vals[k],vals[2*k+1]):
+                
+
+    def insertNum(self,val):
+        
 
 
 
